44e95d5d377961a435db71bda27044f05fb5c49c
Spence Robison <spence@MacBook-Pro-2.local>
2019-09-26 12:19:03 -0600

    made add fucntion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "thousand sunny",
+      newTodo: "",
+      todos: [
+        {
+          title: "Learn React",
+          done: false
+        },
+        {
+          title: "Sleep",
+          done: false
+        }
+      ]
+    };
+
+    this.formSubmitted = this.formSubmitted.bind(this);
+    this.handleTodoChange = this.handleTodoChange.bind(this);
+  }
+  handleTodoChange(e) {
+    this.setState({
+      newTodo: e.target.value
+    });
+  }
+
+  formSubmitted(e) {
+    e.preventDefault();
+    this.setState({
+      newTodo: "",
+      todos: [
+        ...this.state.todos,
+        {
+          title: this.state.newTodo,
+          done: false
+        }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <form onSubmit={this.formSubmitted}>
+          <label htmlFor="new-todo">Add New Item</label>
+          <input
+            id="new-todo"
+            name="newTodo"
+            onChange={this.handleTodoChange}
+            placeholder="new todo"
+            value={this.state.newTodo}
+            style={{ border: "1px solid black" }}
+          />
+          <button type="submit">add todo item</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li key={todo.title}>{todo.title}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
